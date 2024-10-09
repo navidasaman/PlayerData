42f659cae868a2e362f9d829cbb7cc3846824c7b
Saman <119083568+navidasaman@users.noreply.github.com>
2024-10-09 13:31:41 +0200

    added logger to cors config file

--- server/src/main/java/com/playerdata/server/config/CorsConfig.java
+++ server/src/main/java/com/playerdata/server/config/CorsConfig.java
@@ -5,15 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // To enable the request from backend to frontend through API calls
 @Configuration
 public class CorsConfig {
     @Value("${cors.allowedOrigins}")
     private String allowedOrigins;
+    private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        logger.info("Configuring CORS with allowed origins: {}", allowedOrigins);
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
