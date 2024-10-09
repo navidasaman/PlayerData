73b8359f1d27f2fa5674c7b31b245975ab54a314
Saman <119083568+navidasaman@users.noreply.github.com>
2024-10-09 22:37:18 +0200

    rendering done by PlayerDataTable and controller logic handled in PlayerData for separation of concern

--- client/src/components/PlayerDataTable.vue
+++ client/src/components/PlayerDataTable.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-full flex justify-center font-sans text-left sm:p-0 p-4">
-    <table class="bg-[#36304a] text-white rounded-t-lg w-full sm:w-[80vw] shadow-lg mt-12 mb-10" >
+    <table
+      class="bg-[#36304a] text-white rounded-t-lg w-full sm:w-[80vw] shadow-lg mt-12 mb-10"
+    >
       <thead>
         <tr>
           <th class="tableHeaders">
@@ -9,7 +11,7 @@
               <font-awesome-icon
                 icon="fa-solid fa-arrow-down"
                 class="cursor-pointer ml-2"
-                v-on:click="toggleSortingByBirthDate()"
+                @click="toggleSortingByDOB" 
                 title="Press for descending order"
               />
             </span>
@@ -17,7 +19,7 @@
               <font-awesome-icon
                 icon="fa-solid fa-arrow-up"
                 class="cursor-pointer ml-2"
-                v-on:click="toggleSortingByBirthDate()"
+                @click="toggleSortingByDOB" 
                 title="Press for ascending order"
               />
             </span>
@@ -28,7 +30,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="player in sortedPlayers" :key="player.id" class="odd:bg-gray-100 even:bg-white text-gray-500">
+        <tr v-for="player in players" :key="player.id" class="odd:bg-gray-100 even:bg-white text-gray-500">
           <td class="tableData">{{ player.birth_date }}</td>
           <td class="tableData">{{ player.first_name }}</td>
           <td class="tableData">{{ player.last_name }}</td>
@@ -41,44 +43,20 @@
 
 <script>
 export default {
-  data() {
-    return {
-      players: [], // will return data that was fetched
-      toggledSorting: true,
-    };
-  },
-  async mounted() {
-    // After page has loaded (mounted)
-    await this.fetchPlayers(); //  the fetchPlayers method will run
-  },
-  computed: {
-    sortedPlayers() {
-      // creates a copy of the original array of json-data based on dob, which is then sorted in asc/desc order
-      return this.players.slice().sort((thisPlayer, otherPlayer) => {
-        const player1 = new Date(thisPlayer.birth_date);
-        const player2 = new Date(otherPlayer.birth_date);
-        return this.toggledSorting ? player2 - player1 : player1 - player2;
-      });
+  // Recieving props from the parent component 'PlayersData' describing its type and that it is required 
+  props: {
+    players: {
+      type: Array,
+      required: true,
+    },
+    toggledSorting: {
+      type: Boolean,
+      required: true,
     },
   },
   methods: {
-    // Fetches the player data
-    async fetchPlayers() {
-      try {
-        const response = await fetch(`${process.env.VUE_APP_API_URL}/players`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        this.players = data; // The JSON-data will be returned in data(){...} if fetch request is successful
-      } catch (error) {
-        console.error("Error fetching player:", error);
-        alert(`${error.message}`);
-      }
-    },
-    toggleSortingByBirthDate() {
-      this.toggledSorting = !this.toggledSorting;
-      console.log(this.toggledSorting);
+    toggleSortingByDOB() {
+      this.$emit("toggleSortingByDOB");
     },
   },
 };
---
+++ client/src/components/PlayersData.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <!-- v-bind prop binding & passing data to child component PlayerDataTable -->
+    <PlayerDataTable      
+      :players="sortedPlayers" 
+      :toggledSorting="toggledSorting" 
+      @toggleSortingByDOB="toggleSortingByBirthDate"
+    />
+  </div>
+</template>
+<script>
+import PlayerDataTable from "./PlayerDataTable.vue";
+
+export default {
+  components: {
+    PlayerDataTable,
+  },
+  data() {
+    return {
+      players: [], // will return data that was fetched
+      toggledSorting: true,
+    };
+  },
+  async mounted() {             // After page has loaded (mounted)
+    await this.fetchPlayers(); //  the fetchPlayers method will run
+  },
+  computed: {
+    // creates a copy of the original array of json-data based on dob, which is then sorted in asc/desc order
+    sortedPlayers() {
+      return this.players.slice().sort((thisPlayer, otherPlayer) => {
+        const player1 = new Date(thisPlayer.birth_date);
+        const player2 = new Date(otherPlayer.birth_date);
+        return this.toggledSorting ? player2 - player1 : player1 - player2;
+      });
+    },
+  },
+  methods: {
+    // Fetches the player data
+    async fetchPlayers() {
+      try {
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/players`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        this.players = data; // The JSON-data will be returned in data(){...} if fetch request is successful
+      } catch (error) {
+        console.error("Error fetching player:", error);
+        alert(`${error.message}`);
+      }
+    },
+    toggleSortingByBirthDate() {
+      this.toggledSorting = !this.toggledSorting;
+      console.log(this.toggledSorting);
+    },
+  },
+};
+</script>
